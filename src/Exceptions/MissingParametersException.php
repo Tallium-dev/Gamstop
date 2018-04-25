34c268059f4a81643759cf05b3ce2e52afda824b
Yaro, the Sofa King <12fcv4@gmail.com>
2018-04-25 17:13:33 +0300

    Extra info in exceptions message

--- src/Exceptions/MissingParametersException.php
+++ src/Exceptions/MissingParametersException.php
@@ -5,7 +5,7 @@ namespace Yaro\Gamstop\Exceptions;
 class MissingParametersException extends GamstopBaseException
 {
 
-    public function __construct($message = 'GAMSTOP: missing parameters', $code = 0, \Throwable $previous = null)
+    public function __construct($message = 'missing parameters', $code = 0, \Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
