2c65cb9b6264f88846029855913df9b3b7873b50
Yaro, the Sofa King <12fcv4@gmail.com>
2018-04-25 17:13:51 +0300

    Extra info in exceptions message

--- src/Exceptions/NonPostCallException.php
+++ src/Exceptions/NonPostCallException.php
@@ -5,7 +5,7 @@ namespace Yaro\Gamstop\Exceptions;
 class NonPostCallException extends GamstopBaseException
 {
 
-    public function __construct($message = 'GAMSTOP: non-POST call', $code = 0, \Throwable $previous = null)
+    public function __construct($message = 'non-POST call', $code = 0, \Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
