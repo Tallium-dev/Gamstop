83de9bf938c794498a4db566afe1bafa4b170d33
Yaro, the Sofa King <12fcv4@gmail.com>
2018-04-25 17:13:14 +0300

    Extra info in exceptions message

--- src/Exceptions/ApiKeyInvalidException.php
+++ src/Exceptions/ApiKeyInvalidException.php
@@ -5,7 +5,7 @@ namespace Yaro\Gamstop\Exceptions;
 class ApiKeyInvalidException extends GamstopBaseException
 {
 
-    public function __construct($message = 'GAMSTOP: API key invalid or IP address not in range', $code = 0, \Throwable $previous = null)
+    public function __construct($message = 'API key invalid or IP address not in range', $code = 0, \Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
