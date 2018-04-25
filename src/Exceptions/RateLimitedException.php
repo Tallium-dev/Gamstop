7219dce20d34f9251e6854864f0e419b5ce3b8a4
Yaro, the Sofa King <12fcv4@gmail.com>
2018-04-25 17:14:11 +0300

    Extra info in exceptions message

--- src/Exceptions/RateLimitedException.php
+++ src/Exceptions/RateLimitedException.php
@@ -5,7 +5,7 @@ namespace Yaro\Gamstop\Exceptions;
 class RateLimitedException extends GamstopBaseException
 {
 
-    public function __construct($message = 'GAMSTOP: rate limited', $code = 0, \Throwable $previous = null)
+    public function __construct($message = 'rate limited', $code = 0, \Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
