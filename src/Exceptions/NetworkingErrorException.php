f115808973c45fb6809f801771eefebc71e03406
Yaro, the Sofa King <12fcv4@gmail.com>
2018-04-25 17:12:45 +0300

    Extra info in exceptions message

--- src/Exceptions/NetworkingErrorException.php
+++ src/Exceptions/NetworkingErrorException.php
@@ -5,9 +5,4 @@ namespace Yaro\Gamstop\Exceptions;
 class NetworkingErrorException extends GamstopBaseException
 {
 
-    public function __construct($message, $code = 0, \Throwable $previous = null)
-    {
-        parent::__construct('GAMSTOP: '. $message, $code, $previous);
-    }
-
 }
